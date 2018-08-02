abf063e5561698d30848d25e6cafd2fca34c4640
Mark Deanil Vicente <markdeanilvicente@outlook.com>
2018-08-02 14:32:17 +0800

    remove unnecessary pages

--- ASPNETWebAppWithSwagger/Controllers/HomeController.cs
+++ ASPNETWebAppWithSwagger/Controllers/HomeController.cs
@@ -15,25 +15,6 @@ namespace ASPNETWebAppWithSwagger.Controllers
             return Redirect("/swagger");
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
